37e91701bd9822ea8cc81126111975fc03caf5a7
DPElia <e.dallapiazza@student.tudelft.nl>
2023-06-05 18:34:22 +0200

    Update main.py

--- atmospheric_vehicle/glider/structure/main.py
+++ atmospheric_vehicle/glider/structure/main.py
@@ -1,3 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+'Masses of different subsystems'
+mass_PW = 1  # [kg], mass of power subsystem, set as dummy for now
+mass_DH = 1  # [kg], mass of data handling subsystem, set as dummy for now
+mass_TTC = 1  # [kg], mass of communication subsystem, set as dummy for now
+mass_TM = 1  # [kg], mass of thermal subsystem, set as dummy for now
+mass_ADCS = 1  # [kg], mass of ADCS subsystem, set as dummy for now
+mass_PLD = 1  # [kg], mass of payload subsystem, set as dummy for now
+
+'Volumes of different subsystems'
+vol_PW = 1  # [m^3], volume of power subsystem, set as dummy for now
+vol_DH = 1  # [m^3], volume of data handling subsystem, set as dummy for now
+vol_TTC = 1  # [m^3], volume of communication subsystem, set as dummy for now
+vol_TM = 1  # [m^3], volume of thermal subsystem, set as dummy for now
+vol_ADCS = 1  # [m^3], volume of ADCS subsystem, set as dummy for now
+vol_PLD = 1  # [m^3], volume of payload subsystem, set as dummy for now
 
 
 if __name__ == "__main__":
