f0a36aa02b4df4eb3306d863c7e4f71d97f53e7d
mdotzdot <m.zambolin@student.tudelft.nl>
2023-06-05 17:32:17 +0200

    Loads implementation
    Implemented the lift and drag forces in the code

--- atmospheric_vehicle/aeroshell/structure/main.py
+++ atmospheric_vehicle/aeroshell/structure/main.py
@@ -1,3 +1,37 @@
+import numpy as np
+
+# Atmospheric Properties
+atm_pressure = ...
+
+
+# Glider Parameters
+d_glider = ...
+h_glider = ...
+
+# Launcher Parameters
+d_launcher = ...
+
+
+# Capsule Properties
+cd_capsule = ...
+d_capsule = ...
+
+
+# Material Properties
+rho_caps = ...
+
+
+def drag(rho, v_entry, d, cd):
+    return 1/2 * rho * v_entry**2 * np.pi * d**2 * cd / 4
+
+
+def lift(rho, v_entry, d, cl):
+    return 1/2 * rho * v_entry**2 * np.pi * d**2 * cl / 4
+
+def axial_stress(L, D, m):
+    ...
+
+
 
 
 if __name__ == "__main__":
