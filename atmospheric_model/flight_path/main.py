7db48840c381578e25532cd1f5df2984b731e751
EpicDoge22 <noahkaijser@gmail.com>
2023-06-06 21:28:08 +0200

    Added loops such that the density changes if the percentage change is too high. Now set to 1%, but that is a placeholder. The same goes for the 170 km depth. Only a function for the density has to be added, after which the range, flight time, and altitude vs time part is done. After only the eigenmotion calculations have to be added.

--- atmospheric_model/flight_path/main.py
+++ atmospheric_model/flight_path/main.py
@@ -8,14 +8,13 @@ C_D = 0.05
 C_L = 0.7
 m = 72  # kg
 V_0 = 23.6  # m/s
-gamma = np.radians(-15)  # rad
 chi = 0  # rad
 V_x_wind = 250  # m/s
 g_u = 8.69  # m/s^2
 
 
 # Functions
-def velocity(t):
+def velocity(t, gamma):
     z = rho * S * C_D / (2 * m)
     a = z * V_0 - g_u * np.sin(gamma)
     b = 2 * z * V_0 * V_0
@@ -23,7 +22,7 @@ def velocity(t):
     return V  # m/s
 
 
-def range(t):
+def range(t, gamma):
     z = rho * S * C_D / (2 * m)
     a = z * V_0 - g_u * np.sin(gamma)
     b = 2 * z * V_0 * V_0
@@ -31,7 +30,7 @@ def range(t):
     return x  # m
 
 
-def height(t):
+def height(t, gamma):
     z = rho * S * C_D / (2 * m)
     a = z * V_0 - g_u * np.sin(gamma)
     b = 2 * z * V_0 * V_0
@@ -39,12 +38,43 @@ def height(t):
     return h  # m
 
 
-t = np.linspace(0, 10, 100)
+def gamma(rho, V_0):
+    z = rho * S * C_D / (2 * m)
+    gamma = np.arcsin(z * V_0 / g_u)
+    return gamma
+
+
+t = [0]
+V = []
+x = []
+h = [0]
+rho_0 = rho()
+density = [rho_0]
 
 if __name__ == "__main__":
-    V = velocity(t)
-    x = range(t)
-    h = height(t)
+    while h[-1] < 170 * 10 ** 3:
+        if rho / rho_0 < 0.01:
+            t.append(t[-1] + 0.1)
+            gamma = gamma(rho_0, V_0)
+            V = velocity(t[-1], gamma)
+            x = range(t[-1], gamma)
+            z = height(t[-1], gamma)
+            density = rho(z)
+
+            V.append(V)
+            x.append(x)
+            density.append(density)
+
+            if h[0] == 0:
+                h[0] = z
+            else:
+                h.append(z)
+
+        else:
+            V_0 = V[-1]
+            rho_0 = density[-1]
+            gamma = gamma(rho_0, V_0)
+
     plt.plot(t, V, label="Velocity")
     plt.plot(t, x, label="Range")
     plt.plot(t, h, label="Depth")
