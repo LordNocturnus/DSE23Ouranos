4dc3102494e30620e6b81661d0087fc48af8a7e9
snuijten <snuijten@tudelft.nl>
2023-06-07 14:24:23 +0200

    total power battery mass

--- atmospheric_vehicle/glider/main.py
+++ atmospheric_vehicle/glider/main.py
@@ -1,3 +1,16 @@
+dod = 1
+n_bat = 0.9
+n_cab = 0.9
+spec_energy = 1000
+
+def tot_p_req(dh_p_req, ttc_p_req,
+        adcs_p_req, pl_p_req)
+    return dh_p_req+ttc_p_req+adcs_p_req+pl_p_req
+
+def m_bat(dh_p_req, ttc_p_req, adcs_p_req,
+        pl_p_req, dod, n_bat spec_energy)
+    tot_p_req() = tot_p_req(dh_p_req, ttc_p_req, adcs_p_req, pl_p_req)
+    return tot_p_req / dod / n_bat / n_cab / spec_energy
 
 
 if __name__ == "__main__":
