58f37b580c6c28967e2946bbbdc9daabf7714f8e
ArthurVDS07 <101700825+ArthurVDS07@users.noreply.github.com>
2023-06-07 17:14:45 +0200

    small updates

--- Orbiter/propulsion/main.py
+++ Orbiter/propulsion/main.py
@@ -1,10 +1,10 @@
 import numpy as np
 g = 9.81 # gravitational constant in m/s^2
-I_sp = 250 # specific impulse in sec
+I_sp = 321 # specific impulse in sec
 m_flow = 2 # fuel mass flow in kg/s
 t_b = 4 # burn time in sec
-m_f = 2898 # final mass spacecraft without propellant, thus dry mass in kg
-dV = 6.7 # delta V require in km/s
+m_f = 2427 # final mass spacecraft without propellant, thus dry mass in kg
+dV = 2000 # delta V require in m/s
 
 # Calculate required thrust:
 def thrust(m_flow, I_sp, g):
@@ -24,5 +24,5 @@ def mass_prop2(m_f, dV, g, I_sp):
 
 if __name__ == "__main__":
 
-    print(mass_prop(t_b, g, I_sp))
+    # print(mass_prop(t_b, g, I_sp))
     print(mass_prop2(m_f, dV, g, I_sp))
