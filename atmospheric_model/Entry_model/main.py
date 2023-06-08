92610daabe4e2493e5e9280ed7124e0c61a7e860
felix <felix@lagarden.de>
2023-06-08 13:47:35 +0200

    atmospheric deceleration model works

--- atmospheric_model/Entry_model/main.py
+++ atmospheric_model/Entry_model/main.py
@@ -15,17 +15,18 @@ class Core:
         self.beta = 0.0
         self.gamma = 0.0
 
-        self.lat = 0.0
-        self.lon = 0.0
         self.radius = 0.0
         self.heading = 0.0
-        self.dlat = 0.0
-        self.dlon = 0.0
 
         self.alt = 0.0
-        self.dis = 0.0
+        self.lat = 0.0
+        self.lon = 0.0
+        self.heat = 0.0
         self._vx = 0.0
+        self.dlat = 0.0
+        self.dlon = 0.0
         self.vy = 0.0
+        self.q = 0.0
         self.ax = 0.0
         self.ay = 0.0
 
@@ -41,7 +42,8 @@ class Core:
                          self.lat,
                          self.lon,
                          self.vy,
-                         self._vx])
+                         self._vx,
+                         self.heat])
 
     @property
     def dy(self):
@@ -49,15 +51,19 @@ class Core:
                          self.dlat,
                          self.dlon,
                          self.ay,
-                         self.ax])
+                         self.ax,
+                         self.q])
 
     @y.setter
     def y(self, y):
         self.alt = y[0]
         self.lat = np.arcsin(np.sin(y[1]))
         self.lon = y[2] % np.pi
+        if self.lon < 0.0:
+            self.lon += 2 * np.pi
         self.vy = y[3]
         self.vx = y[4]
+        self.heat = y[5]
 
     @vx.setter
     def vx(self, other):
@@ -69,8 +75,8 @@ class Core:
 def uranus_entry_sim(alt, lat, lon, vel, gamma, heading, mass, beta, c_l, c_d):
     gram =GRAM.GRAM()
     gram.altitudes = np.array([0.0])
-    gram.lat = np.array([lat])
-    gram.long = np.array([lon])
+    gram.lat = np.array([np.rad2deg(lat)])
+    gram.long = np.array([np.rad2deg(lon)])
     gram.time = np.array([0.0])
     gram.run()
 
@@ -89,7 +95,7 @@ def uranus_entry_sim(alt, lat, lon, vel, gamma, heading, mass, beta, c_l, c_d):
 
     vx = vel * np.sin(gamma)
     vy = vel * np.cos(gamma)
-    y0 = np.asarray([alt - core.radius * 1000, lat, lon, vx, vy])
+    y0 = np.asarray([alt - core.radius * 1000, lat, lon, vx, vy, 0.0])
 
     def rhs(t, y):
         core.y = y
@@ -102,27 +108,22 @@ def uranus_entry_sim(alt, lat, lon, vel, gamma, heading, mass, beta, c_l, c_d):
         gram.run()
         core.radius = gram.data.LatitudeRadius_km[0]
         core.ax = -1 / 2 * gram.data.Density_kgm3[0] / (core.beta * np.cos(alpha)) * \
-                  core.vx ** 2 * (core.c_l / core.c_d + np.tan(alpha))
+                  core.vx ** 2 * (core.c_l / core.c_d * np.tan(alpha) + 1)
         core.ay = 1 / 2 * gram.data.Density_kgm3[0] / (core.beta * np.sin(alpha)) * \
-                  core.vy ** 2 * (-core.c_l / core.c_d + 1 / np.tan(alpha)) - gram.data.Gravity_ms2[0]
+                  core.vy ** 2 * (core.c_l / core.c_d * 1 / np.tan(alpha) - 1) - gram.data.Gravity_ms2[0]
+        core.q = (core.vx ** 2 + core.vy ** 2) ** (3/2) * gram.data.Density_kgm3[0]
         return core.dy
-
-    def event_dis(t, y):
-        return 50000000 - y[1]
-
-    event_dis.terminal = True
 
     def event_alt(t, y):
         print("t: ", t)
         print("alt: ", y[0])
-        print("lat: ", np.rad2deg(y[1]))
-        print("lon: ", np.rad2deg(y[2]))
+        print("vx: ", y[4])
         return y[0]
 
     event_alt.terminal = True
 
     sol = solve_ivp(rhs, [core.t, 18000], y0,
-                    events=[event_dis, event_alt],
+                    events=event_alt,
                     max_step=1, method="LSODA",
                     rtol=1e-3, atol=1e-6)
 
@@ -130,14 +131,46 @@ def uranus_entry_sim(alt, lat, lon, vel, gamma, heading, mass, beta, c_l, c_d):
 
 
 if __name__ == "__main__":
-    example = [3.02877105e+07, -6.40748300e-02, -1.63500310e+00,  1.93919454e+04, -4.15342314e-01, -2.35413606e+00]
+    example_5000 = [3.02877105e+07, -6.40748300e-02, -1.63500310e+00,  1.93919454e+04, -4.15342314e-01, -2.35413606e+00]
 
-    test = uranus_entry_sim(example[0], np.rad2deg(example[1]), np.rad2deg(example[2]), example[3], np.deg2rad(-10),
-                            example[5], 500, 125, 0, 1)
+    example_2500 = [2.78349356e+07, -2.20565560e-01, -2.91545691e+00,  2.02425574e+04, -3.05261011e-01, 2.33104808e+00]
 
-    plt.plot(test.y[1] / 1000, test.y[0] / 1000)
+    test = uranus_entry_sim(example_2500[0]-1700000, example_2500[1], example_2500[2] + 2 * np.pi, example_2500[3],
+                            np.deg2rad(-10), example_2500[5], 500, 125, 0, 1)
+
+    gram = GRAM.GRAM()
+    gram.altitudes = test.y[0] / 1000
+    gram.lat = np.rad2deg(np.arcsin(np.sin(test.y[1])))
+    gram.long = np.rad2deg(test.y[2] % np.pi)
+    gram.long[gram.long < 0.0] += 2 * np.pi
+    gram.time = test.t
+    gram.run()
+
+    plt.plot(test.y[3] / 1000, test.y[0] / 1000, label="Vx")
+    plt.plot(test.y[4] / 1000, test.y[0] / 1000, label="Vy")
+    plt.plot(np.sqrt(test.y[3] ** 2 + test.y[4] ** 2) / 1000, test.y[0] / 1000, label="V_total")
+    plt.legend()
     plt.show()
-    plt.plot(test.y[2] / 1000, test.y[0] / 1000)
+
+    plt.plot(test.t, test.y[0] / 1000, label="alt")
+    plt.legend()
     plt.show()
-    plt.plot(test.y[3] / 1000, test.y[0] / 1000)
+
+    plt.plot(test.t, (test.y[3] ** 2 + test.y[4] ** 2) ** (3/2) * np.asarray(gram.data.Density_kgm3), label="heat flux")
+    plt.legend()
+    plt.show()
+
+    plt.plot(test.t, test.y[5], label="heat load")
+    plt.legend()
+    plt.show()
+
+    alpha = np.arctan2(test.y[4], test.y[3])
+    ax = -1 / 2 * np.asarray(gram.data.Density_kgm3) / (125 * np.cos(alpha)) * \
+         test.y[3] ** 2 * (0 / 1 * np.tan(alpha) + 1)
+    ay = 1 / 2 * np.asarray(gram.data.Density_kgm3) / (125 * np.sin(alpha)) * \
+         test.y[4] ** 2 * (0 / 1 * 1 / np.tan(alpha) - 1) - np.asarray(gram.data.Gravity_ms2)
+    plt.plot(ax, test.y[0] / 1000, label="ax")
+    plt.plot(ay, test.y[0] / 1000, label="ay")
+    plt.plot(np.sqrt(ax ** 2 + ay ** 2), test.y[0] / 1000, label="a_total")
+    plt.legend()
     plt.show()
