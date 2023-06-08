766caaa371b010ac941315211d335af0edf01b8a
snuijten <snuijten@tudelft.nl>
2023-06-08 11:29:43 +0200

    added volume

--- atmospheric_vehicle/glider/main.py
+++ atmospheric_vehicle/glider/main.py
@@ -1,7 +1,8 @@
 dod = 1
 n_bat = 0.9
 n_cab = 0.9
-spec_energy = 280
+vol_energy = 400e3
+spec_energy = 250
 margin = 0.5
 days=3
 
@@ -24,10 +25,17 @@ def f_m_bat(tot_p_req, t, dod, n_bat, n_cab, spec_energy):
     m_bat = tot_p_req * t / dod / n_bat / n_cab / spec_energy
     return m_bat
 
+def f_v_bat(tot_p_req, t, dod, n_bat, n_cab, vol_energy):
+    v_bat = tot_p_req * t / dod / n_bat / n_cab / vol_energy
+    return v_bat
+
 if __name__ == "__main__":
     tot_p_req = f_tot_p_req(dh_p_req, ttc_p_req, adcs_p_req, pl_p_req, tm_p_req)
     t = f_t(days)
     m_bat = f_m_bat(tot_p_req, t, dod, n_bat, n_cab, spec_energy)
     m_bat = m_bat*(1+margin)
+    v_bat = f_v_bat(tot_p_req, t, dod, n_bat, n_cab, spec_energy)
+    v_bat = v_bat*(1+margin)
     print("Required energy [kWh]:", tot_p_req*t/1000)
     print("Battery mass [kg]:", m_bat)
+    print("Battery mass [kg]:", v_bat)
