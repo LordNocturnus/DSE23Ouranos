46134cc00315f8c8b821351ce8b763234d3f2034
snuijten <snuijten@tudelft.nl>
2023-06-09 13:55:15 +0200

    corrections

--- atmospheric_vehicle/glider/comms/main.py
+++ atmospheric_vehicle/glider/comms/main.py
@@ -1,12 +1,12 @@
 import numpy as np
 n_tx = 0.18 #-
-n_rx = 0.6 #-
+n_rx = 0.55 #-
 
 def unit_to_db(unit):
-    return 20*np.log10(unit)
+    return 10*np.log10(unit)
 
 def db_to_unit(db):
-    return 10**(db/20)
+    return 10**(db/10)
 
 def l_fs(freq, r):
     wavelength = 3e8 / freq
@@ -14,7 +14,7 @@ def l_fs(freq, r):
     return l_fs
 
 def s(r):
-    s = 2*np.pi*r*r
+    s = np.pi*r*r
     return s
 
 def p_rx(p_tx, g_tx, l_tx, l_fs, l_m, g_rx, l_rx): #input in dB
@@ -29,27 +29,34 @@ def n_0(t_s): #first estimation, update with https://deepspace.jpl.nasa.gov/dsnd
     return 1.380e-23*t_s
 
 def energy_per_bit_criteria(e_b, n_0):
-    return print("Energy per bit criteria:", (e_b / n_0 >= 10))
-
+    print(e_b/n_0)
+    print("Energy per bit criteria:", (e_b / n_0 >= 10))
 
 if __name__ == "__main__":
     g_tx_ao = unit_to_db(2) #dB
+    print("g_tx:",g_tx_ao)
     l_tx = unit_to_db(1/n_tx) #dB
-    freq_ao = 3e9 #Hz
+    print("l_tx:",l_tx)
+    freq_ao = 400e6 #Hz
     max_r_ao = 10000 #km
     l_fs_ao = l_fs(freq_ao, max_r_ao*1000) #dB
-    r_rx_ao = 0.01 #m
+    print("l_fs:",l_fs_ao)
+    r_rx_ao = 0.5 #m
     s_rx_ao = s(r_rx_ao) #m^2
     g_rx_ao = unit_to_db(s_rx_ao) #dB
+    print("g_rx:",g_rx_ao)
     l_rx = unit_to_db(1/n_rx) #dB
+    print("l_rx:",l_rx)
     l_m = 0
-    p_tx_ao = 1000 #W
+    p_tx_ao = 100 #W
     p_tx_ao = unit_to_db(p_tx_ao) #dB
+    print("p_tx:",p_tx_ao)
     p_rx_ao = p_rx(p_tx_ao, g_tx_ao, l_tx, l_fs_ao, l_m, g_rx_ao, l_rx)
     bit_rate_ao = 2455 #bps
-    print(db_to_unit(p_rx_ao))
+    print("p_rx:",db_to_unit(p_rx_ao))
     e_b_ao = e_b(db_to_unit(p_rx_ao), bit_rate_ao)
-    print(e_b_ao)
-    n_o = n_0(1000)
-    print(n_o)
-    energy_per_bit_criteria(e_b_ao, n_o)
+    print("e_b:", e_b_ao)
+    n_0 = n_0(103.15)
+    print("n_0:", n_0)
+    print("n_0 [dB]:", unit_to_db(n_0))
+    energy_per_bit_criteria(e_b_ao, n_0)
