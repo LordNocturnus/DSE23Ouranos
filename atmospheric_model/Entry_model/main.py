9105f2a835e406b2e240720d4ecde5efd5dd11df
LordNocturnus <41686329+LordNocturnus@users.noreply.github.com>
2023-06-10 18:06:44 +0200

    started setup of GRAM integration

--- atmospheric_model/Entry_model/main.py
+++ atmospheric_model/Entry_model/main.py
@@ -1,7 +1,7 @@
-# Load standard modules
-import math
-import numpy as np
 from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import math
 
 # Load tudatpy modules
 from tudatpy.kernel.interface import spice
@@ -11,20 +11,42 @@ from tudatpy.kernel.astro import element_conversion
 from tudatpy.kernel import constants
 from tudatpy.util import result2array
 
+from atmospheric_model import GRAM
 
-def entry_sim(mass, drag_coefficient, diameter):
-    # Manually create (empty) settings for a list of bodies, with origin Uranus
-    body_settings = environment_setup.BodyListSettings(frame_origin="Uranus")
+
+def entry_sim(mass, drag_coefficient, diameter, lat, lon, acc=1):
+    # define bodies in simulation
+    bodies_to_create = ["Uranus"]
+
+    # create body settings dictionary
+    global_frame_origin = "Uranus"
+    global_frame_orientation = "J2000"
+    body_settings = environment_setup.get_default_body_settings(
+        bodies_to_create, global_frame_origin, global_frame_orientation)
 
     # Modify the gravity field of Uranus to match GRAM
     body_settings.get("Uranus").gravity_field_settings = environment_setup.gravity_field.central_spice()
-    body_settings.get("Uranus").atmosphere_settings = environment_setup.atmosphere.tabulated(__file__[:-7]+"atmos_data-csv",
-                                                                                             ["tabulated_density",
-                                                                                              "tabulated_pressure",
-                                                                                              "tabulated_temperature",
-                                                                                              "tabulated_gas_constant",
-                                                                                              "tabulated_specific_heat_ratio",
-                                                                                              "tabulated_molar_mass"])
+
+    # Modify the atmosphere to match GRAM
+    gram = GRAM.GRAM()
+    gram.altitudes = np.arange(7000, -290-acc, -acc)
+    gram.lat = np.full_like(gram.altitudes, lat)
+    gram.long = np.full_like(gram.altitudes, lon)
+    gram.time = np.zeros_like(gram.altitudes)
+    gram.run()
+
+    atmos = {"altitude": np.asarray(gram.data.Height_km) * 1000,
+             "density": np.asarray(gram.data.Density_kgm3),
+             "pressure": np.asarray(gram.data.Pressure_Pa),
+             "temperature": np.asarray(gram.data.Temperature_K),
+             "gas_constant": np.asarray(gram.data.Height_km),
+             "specific_heat_ratio": np.asarray(gram.data.Height_km),
+             "molar_mass": np.asarray(gram.data.Height_km),}
+
+    body_settings.get("Uranus").atmosphere_settings = environment_setup.atmosphere.tabulated(
+        __file__[:-7]+"atmos_data-csv", ["tabulated_density", "tabulated_pressure", "tabulated_temperature",
+                                         "tabulated_gas_constant", "tabulated_specific_heat_ratio",
+                                         "tabulated_molar_mass"])
     # Create system of bodies from the body settings
     bodies = environment_setup.create_system_of_bodies(body_settings)
 
@@ -36,11 +58,9 @@ def entry_sim(mass, drag_coefficient, diameter):
     # Create aerodynamic coefficient interface settings, and add to vehicle
     aero_coefficient_settings = environment_setup.aerodynamic_coefficients.constant(
         reference_area=np.pi * (diameter / 2) ** 2,
-        constant_force_coefficient=[drag_coefficient, 0, 0]
-    )
-    environment_setup.add_aerodynamic_coefficient_interface(
-        bodies, "Capsule", aero_coefficient_settings)
+        constant_force_coefficient=[drag_coefficient, 0, 0])
+    environment_setup.add_aerodynamic_coefficient_interface(bodies, "Capsule", aero_coefficient_settings)
 
 
 if __name__ == "__main__":
-    entry_sim(1550, 1.53, 4.5)
+    entry_sim(1550, 1.53, 4.5, 0.0, 0.0)
