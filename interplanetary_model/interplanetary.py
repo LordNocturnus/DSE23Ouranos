443b4c1d1acd2ae9f247bd1c18772b6cf02ce5f0
Jeroen Dijkstra <jeroen1066@gmail.com>
2023-06-12 17:22:19 +0200

    implemented as class, now works for arrival flyby.

--- interplanetary_model/interplanetary.py
+++ interplanetary_model/interplanetary.py
@@ -39,8 +39,9 @@ def convert_trajectory_parameters (transfer_trajectory_object: tudatpy.kernel.tr
     for i in range(transfer_trajectory_object.number_of_legs):
         leg_free_parameters.append( [ ] )
     # One empty array for each node
-    for i in range(transfer_trajectory_object.number_of_nodes):
+    for i in range(transfer_trajectory_object.number_of_nodes-1):
         node_free_parameters.append( [ ] )
+    node_free_parameters.append([1,1,1])
 
     return node_times, leg_free_parameters, node_free_parameters
 
@@ -53,7 +54,7 @@ class TransferTrajectoryProblem:
     def __init__(self,
                  transfer_trajectory_object: tudatpy.kernel.trajectory_design.transfer_trajectory.TransferTrajectory,
                  departure_date_lb: float, # Lower bound on departure date
-                 departure_date_up: float, # Upper bound on departure date
+                 departure_date_ub: float, # Upper bound on departure date
                  legs_tof_lb: np.ndarray, # Lower bounds of each leg's time of flight
                  legs_tof_ub: np.ndarray): # Upper bounds of each leg's time of flight
         """
@@ -136,172 +137,203 @@ class TransferTrajectoryProblem:
 # Define transfer trajectory properties
 ###########################################################################
 
-# Define the central body
-central_body = "Sun"
 
-# Define order of bodies (nodes)
-transfer_body_order = ['Earth', 'Venus', 'Earth', 'Earth', 'Jupiter', 'Uranus']
+class interplanetary_trajectory:
+    def __init__(self,transfer_body_order,departure_orbit,target_periapsis,launch_interval):
 
-# Define departure orbit
-departure_semi_major_axis = np.inf
-departure_eccentricity = 0
+        # Define the central body
+        self.central_body = "Sun"
 
-# Define insertion orbit
-arrival_semi_major_axis = 1.0895e8 / 0.02
-arrival_eccentricity = 0.98
+        # Define order of bodies (nodes)
+        transfer_body_order = ['Earth', 'Venus', 'Earth', 'Earth', 'Jupiter', 'Uranus']
 
-# Create simplified system of bodies
-bodies = environment_setup.create_simplified_system_of_bodies()
+        # Define departure orbit
+        self.departure_semi_major_axis = departure_orbit[0]
+        self.departure_eccentricity = departure_orbit[1]
+
+
+        # Create simplified system of bodies
+        self.bodies = environment_setup.create_simplified_system_of_bodies()
 
 # Define the trajectory settings for both the legs and at the nodes
-transfer_leg_settings, transfer_node_settings = transfer_trajectory.mga_settings_unpowered_unperturbed_legs(
-    transfer_body_order,
-    departure_orbit=(departure_semi_major_axis, departure_eccentricity),minimum_pericenters = {'Earth': 6678000.0, 'Jupiter': 600000000.0, 'Mars': 3689000.0, 'Mercury': 2740000.0, 'Saturn': 70000000.0, 'Venus': 6351800.0, 'Uranus':25362000})
-#    arrival_orbit=(arrival_semi_major_axis, arrival_eccentricity))
+        self.transfer_leg_settings, self.transfer_node_settings = transfer_trajectory.mga_settings_unpowered_unperturbed_legs(
+            transfer_body_order,
+            departure_orbit=(self.departure_semi_major_axis, self.departure_eccentricity),minimum_pericenters = {'Earth': 6678000.0, 'Jupiter': 600000000.0, 'Mars': 3689000.0, 'Mercury': 2740000.0, 'Saturn': 70000000.0, 'Venus': 6351800.0, 'Uranus':target_periapsis})
+        #    arrival_orbit=(arrival_semi_major_axis, arrival_eccentricity))
 
-# Create the transfer calculation object
-transfer_trajectory_object = transfer_trajectory.create_transfer_trajectory(
-    bodies,
-    transfer_leg_settings,
-    transfer_node_settings,
-    transfer_body_order,
-    central_body)
+        # Create the transfer calculation object
+        self.transfer_trajectory_object = transfer_trajectory.create_transfer_trajectory(
+            self.bodies,
+            self.transfer_leg_settings,
+            self.transfer_node_settings,
+            transfer_body_order,
+            self.central_body)
 
-# Lower and upper bound on departure date
-departure_date_lb = 11000 * constants.JULIAN_DAY
-departure_date_ub = 14000 * constants.JULIAN_DAY
+        # Lower and upper bound on departure date
+#        self.departure_date_lb = 11000 * constants.JULIAN_DAY
+#        self.departure_date_ub = 14000 * constants.JULIAN_DAY
+        self.departure_date_lb = launch_interval[0]
+        self.departure_date_ub = launch_interval[1]
 
-# List of lower and upper on time of flight for each leg
-legs_tof_lb = np.zeros(5)
-legs_tof_ub = np.zeros(5)
-# Venus first fly-by
-legs_tof_lb[0] = 30 * constants.JULIAN_DAY
-legs_tof_ub[0] = 400 * constants.JULIAN_DAY
-# Earth first fly-by
-legs_tof_lb[1] = 100 * constants.JULIAN_DAY
-legs_tof_ub[1] = 470 * constants.JULIAN_DAY
-# Earth second fly-by
-legs_tof_lb[2] = 100 * constants.JULIAN_DAY
-legs_tof_ub[2] = 500 * constants.JULIAN_DAY
-# Jupiter fly-by
-legs_tof_lb[3] = 400 * constants.JULIAN_DAY
-legs_tof_ub[3] = 2000 * constants.JULIAN_DAY
-# Uranus fly-by
-legs_tof_lb[4] = 15*365 * constants.JULIAN_DAY
-legs_tof_ub[4] = 25*365 * constants.JULIAN_DAY
+        # List of lower and upper on time of flight for each leg
+        legs_tof_lb = np.zeros(len(transfer_body_order)-1)
+        legs_tof_ub = np.zeros(len(transfer_body_order)-1)
+        for i in range(len(legs_tof_lb)):
+        # Venus first fly-by
+            if transfer_body_order[i+1] in ["Jupiter","Saturn"]:
+                legs_tof_lb[i] = 400 * constants.JULIAN_DAY
+                legs_tof_ub[i] = 2000 * constants.JULIAN_DAY                
+            elif (transfer_body_order[i+1] in ['Venus','Earth','Mars'] and transfer_body_order[i] in ['Venus','Earth','Mars']):
+                legs_tof_lb[i] = 30 * constants.JULIAN_DAY
+                legs_tof_ub[i] = 500 * constants.JULIAN_DAY
+            elif transfer_body_order[i+1] in ['Venus','Earth','Mars'] and transfer_body_order[i] in ['Jupiter','Saturn']:
+                legs_tof_lb[i] = 100 * constants.JULIAN_DAY
+                legs_tof_ub[i] = 1000 * constants.JULIAN_DAY   
+            elif transfer_body_order[i+1] == "Uranus":
+                legs_tof_lb[i] = 3 * constants.JULIAN_YEAR
+                legs_tof_ub[i] = 20 * constants.JULIAN_YEAR
 
-###########################################################################
-# Setup optimization
-###########################################################################
-# Initialize optimization class
-optimizer = TransferTrajectoryProblem(transfer_trajectory_object,
-                                        departure_date_lb,
-                                        departure_date_ub,
-                                        legs_tof_lb,
-                                        legs_tof_ub)
+            self.legs_tof_lb = legs_tof_lb
+            self.legs_tof_ub = legs_tof_ub
 
-# Creation of the pygmo problem object
-prob = pg.problem(optimizer)
 
-# To print the problem's information: uncomment the next line
-# print(prob)
+        ###########################################################################
+        # Setup optimization
+        ###########################################################################
+        # Initialize optimization class
 
-# Define number of generations per evolution
-number_of_generations = 1
+    def optimize(self,repeats):
+        optimizer = TransferTrajectoryProblem(self.transfer_trajectory_object,
+                                                self.departure_date_lb,
+                                                self.departure_date_ub,
+                                                self.legs_tof_lb,
+                                                self.legs_tof_ub)
 
-# Fix seed
-optimization_seed = 4444
+        # Creation of the pygmo problem object
+        prob = pg.problem(optimizer)
 
-# Create pygmo algorithm object
-algo = pg.algorithm(pg.de(gen=number_of_generations, seed=optimization_seed, F=0.8))
+        # To print the problem's information: uncomment the next line
+        # print(prob)
 
-# To print the algorithm's information: uncomment the next line
-# print(algo)
+        # Define number of generations per evolution
+        number_of_generations = 1
+        bestdv = 1e20
+        # Fix seed
+        for j in range(repeats):
+            #print (j)
+            # Fix seed
+            optimization_seed = np.random.randint(0,1000)
 
-# Set population size
-population_size = 20
+            optimization_seed = int(optimization_seed)
 
-# Create population
-pop = pg.population(prob, size=population_size, seed=optimization_seed)
+            # Create pygmo algorithm object
+            algo = pg.algorithm(pg.de(gen=number_of_generations, seed=optimization_seed, F=0.55,CR=0.7,variant=4))
 
-###########################################################################
-# Run optimization
-###########################################################################
+            # To print the algorithm's information: uncomment the next line
+            # print(algo)
 
-# Set number of evolutions
-number_of_evolutions = 800
+            # Set population size
+            population_size = 20
 
-# Initialize empty containers
-individuals_list = []
-fitness_list = []
+            # Create population
+            pop = pg.population(prob, size=population_size, seed=optimization_seed)
 
-for i in range(number_of_evolutions):
+            ###########################################################################
+            # Run optimization
+            ###########################################################################
 
-    pop = algo.evolve(pop)
+            # Set number of evolutions
+            self.number_of_evolutions = 800
 
-    # individuals save
-    individuals_list.append(pop.champion_x)
-    fitness_list.append(pop.champion_f)
+            # Initialize empty containers
+            individuals_list = []
+            self.fitness_list = []
 
-print('The optimization has finished')
+            for i in range(self.number_of_evolutions):
 
-###########################################################################
-# Results post-processing
-###########################################################################
+                pop = algo.evolve(pop)
 
-# Extract the best individual
-print('\n########### CHAMPION INDIVIDUAL ###########\n')
-print('Total Delta V [m/s]: ', pop.champion_f[0])
-best_decision_variables = pop.champion_x/constants.JULIAN_DAY
-print('Departure time w.r.t J2000 [days]: ', best_decision_variables[0])
-print('Earth-Venus time of flight [days]: ', best_decision_variables[1])
-print('Venus-Earth time of flight [days]: ', best_decision_variables[2])
-print('Earth-Earth time of flight [days]: ', best_decision_variables[3])
-print('Earth-Jupiter time of flight [days]: ', best_decision_variables[4])
-print('Jupiter-Uranus time of flight [days]: ', best_decision_variables[5])
+                # individuals save
+                individuals_list.append(pop.champion_x)
+                self.fitness_list.append(pop.champion_f)
+            if pop.champion_f < bestdv:
+                bestdv = pop.champion_f
+                bestpop = pop
 
-# Plot fitness over generations
-fig, ax = plt.subplots(figsize=(8, 4))
-ax.plot(np.arange(0, number_of_evolutions), np.float_(fitness_list) / 1000, label='Function value: Feval')
-# Plot champion
-champion_n = np.argmin(np.array(fitness_list))
-ax.scatter(champion_n, np.min(fitness_list) / 1000, marker='x', color='r', label='All-time champion', zorder=10)
+        self.pop = bestpop
 
-# Prettify
-ax.set_xlim((0, number_of_evolutions))
-ax.set_ylim([4, 25])
-ax.grid('major')
-ax.set_title('Best individual over generations', fontweight='bold')
-ax.set_xlabel('Number of generation')
-ax.set_ylabel(r'$\Delta V [km/s]$')
-ax.legend(loc='upper right')
-plt.tight_layout()
-plt.legend()
-plt.show()
+    def plot(self):
+        ###########################################################################
+        # Results post-processing
+        ###########################################################################
 
-# Reevaluate the transfer trajectory using the champion design variables
-node_times, leg_free_parameters, node_free_parameters = convert_trajectory_parameters(transfer_trajectory_object, pop.champion_x)
-transfer_trajectory_object.evaluate(node_times, leg_free_parameters, node_free_parameters)
+        # Extract the best individual
+        print('\n########### CHAMPION INDIVIDUAL ###########\n')
+        print('Total Delta V [m/s]: ', self.pop.champion_f[0])
+        best_decision_variables = self.pop.champion_x/constants.JULIAN_DAY
+        print('Departure time w.r.t J2000 [days]: ', best_decision_variables[0])
+        print('Earth-Venus time of flight [days]: ', best_decision_variables[1])
+        print('Venus-Earth time of flight [days]: ', best_decision_variables[2])
+        print('Earth-Earth time of flight [days]: ', best_decision_variables[3])
+        print('Earth-Jupiter time of flight [days]: ', best_decision_variables[4])
+        print('Jupiter-Uranus time of flight [days]: ', best_decision_variables[5])
 
-# Extract the state history
-state_history = transfer_trajectory_object.states_along_trajectory(500)
-fly_by_states = np.array([state_history[node_times[i]] for i in range(len(node_times))])
-state_history = result2array(state_history)
-au = 1.5e11
+        # Plot fitness over generations
+        fig, ax = plt.subplots(figsize=(8, 4))
+        ax.plot(np.arange(0, self.number_of_evolutions), np.float_(self.fitness_list) / 1000, label='Function value: Feval')
+        # Plot champion
+        champion_n = np.argmin(np.array(self.fitness_list))
+        ax.scatter(champion_n, np.min(self.fitness_list) / 1000, marker='x', color='r', label='All-time champion', zorder=10)
 
-# Plot the state history
-fig = plt.figure(figsize=(8,5))
-ax = fig.add_subplot(111)
-ax.plot(state_history[:, 1] / au, state_history[:, 2] / au)
-ax.scatter(fly_by_states[0, 0] / au, fly_by_states[0, 1] / au, color='blue', label='Earth departure')
-ax.scatter(fly_by_states[1, 0] / au, fly_by_states[1, 1] / au, color='green', label='Venus fly-by')
-ax.scatter(fly_by_states[2, 0] / au, fly_by_states[2, 1] / au, color='green')
-ax.scatter(fly_by_states[3, 0] / au, fly_by_states[3, 1] / au, color='brown', label='Earth fly-by')
-ax.scatter(fly_by_states[4, 0] / au, fly_by_states[4, 1] / au, color='red', label='Jupiter fly-by')
-ax.scatter(fly_by_states[5, 0] / au, fly_by_states[5, 1] / au, color='grey', label='Saturn arrival')
-ax.scatter([0], [0], color='orange', label='Sun')
-ax.set_xlabel('x wrt Sun [AU]')
-ax.set_ylabel('y wrt Sun [AU]')
-ax.set_aspect('equal')
-ax.legend(bbox_to_anchor=[1, 1])
-plt.show()
+        # Prettify
+        ax.set_xlim((0, self.number_of_evolutions))
+        ax.set_ylim([4, 25])
+        ax.grid('major')
+        ax.set_title('Best individual over generations', fontweight='bold')
+        ax.set_xlabel('Number of generation')
+        ax.set_ylabel(r'$\Delta V [km/s]$')
+        ax.legend(loc='upper right')
+        plt.tight_layout()
+        plt.legend()
+        plt.show()
+
+        # Reevaluate the transfer trajectory using the champion design variables
+        node_times, leg_free_parameters, node_free_parameters = convert_trajectory_parameters(self.transfer_trajectory_object, self.pop.champion_x)
+        self.transfer_trajectory_object.evaluate(node_times, leg_free_parameters, node_free_parameters)
+
+        # Extract the state history
+        state_history = self.transfer_trajectory_object.states_along_trajectory(500)
+        fly_by_states = np.array([state_history[node_times[i]] for i in range(len(node_times))])
+        state_history = result2array(state_history)
+        au = 1.5e11
+
+        # Plot the state history
+        fig = plt.figure(figsize=(8,5))
+        ax = fig.add_subplot(111)
+        ax.plot(state_history[:, 1] / au, state_history[:, 2] / au)
+        ax.scatter(fly_by_states[0, 0] / au, fly_by_states[0, 1] / au, color='blue', label='Earth departure')
+        ax.scatter(fly_by_states[1, 0] / au, fly_by_states[1, 1] / au, color='green', label='Venus fly-by')
+        ax.scatter(fly_by_states[2, 0] / au, fly_by_states[2, 1] / au, color='green')
+        ax.scatter(fly_by_states[3, 0] / au, fly_by_states[3, 1] / au, color='brown', label='Earth fly-by')
+        ax.scatter(fly_by_states[4, 0] / au, fly_by_states[4, 1] / au, color='red', label='Jupiter fly-by')
+        ax.scatter(fly_by_states[5, 0] / au, fly_by_states[5, 1] / au, color='grey', label='Saturn arrival')
+        ax.scatter([0], [0], color='orange', label='Sun')
+        ax.set_xlabel('x wrt Sun [AU]')
+        ax.set_ylabel('y wrt Sun [AU]')
+        ax.set_aspect('equal')
+        ax.legend(bbox_to_anchor=[1, 1])
+        plt.show()
+    def output(self):
+        print("The departure burn will be",self.pop.champion_f[0],'delta v')
+
+
+
+if __name__ == "__main__":
+    print("Hello, world")
+    planets = ['Earth','Venus','Earth','Earth','Jupiter','Uranus']
+    earthorbit = (10000000,0)
+    periapsis = 30000000
+    launching = (30*constants.JULIAN_YEAR,40*constants.JULIAN_YEAR)
+    trajectory = interplanetary_trajectory(planets,earthorbit,periapsis,launching)
+    trajectory.optimize(25)
+    trajectory.plot()
