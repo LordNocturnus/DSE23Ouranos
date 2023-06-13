f1ac22d50f103963481846a316c08385c4d61a4f
nkaijser <nkaijser@student.tudelft.nl>
2023-06-13 09:03:36 +0200

    Created test_main file

---
+++ Orbiter/structure/test_main.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+from Orbiter.structure.main import *
+
+class Test(TestCase):
+    def test_axial_loads(self):
+        self.fail()
+
+
+
+    # def test_lateral_loads(self):
+    #     self.fail()
+    #
+    #
+    #
+    # def test_t_hoop_sphere(self):
+    #     self.fail()
+    #
+    #
+    #
+    # def test_t_hoop_cylind(self):
+    #     self.fail()
+    #
+    #
+    #
+    # def test_geometry_mass(self):
+    #     self.fail()
+    #
+    #
+    #
+    # def test_final_architecture(self):
+    #     self.fail()
+    #
+    #
+    #
+    # def test_natural_frequency(self):
+    #     self.fail()
