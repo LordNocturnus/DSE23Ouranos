05c56f2607f4fb812b9b162687e1b8aaf183aa05
LordNocturnus <41686329+LordNocturnus@users.noreply.github.com>
2023-06-13 07:31:30 +0200

    switched entry sim to interpolated gram

--- atmospheric_model/Entry_model/main.py
+++ atmospheric_model/Entry_model/main.py
@@ -62,8 +62,10 @@ def entry_sim(mass, drag_coefficient, diameter, alt, lat, lon, speed, flight_pat
     atmos = pd.DataFrame(atmos, dtype=float)
     # atmos.to_csv(_path + "/atmos_data.csv", index=False)
 
-    def density(h):
-        return np.asarray(gram.data.Density_kgm3[gram.data.Height_km <= h / 1000])[0]
+    density = sp.interpolate.interp1d(gram.data.Height_km * 1000, gram.data.Density_kgm3)
+
+    """def density(h):
+        return np.asarray(gram.data.Density_kgm3[gram.data.Height_km <= h / 1000])[0]"""
 
     constant_temperature = np.asarray(gram.data.Density_kgm3[gram.data.Height_km <= 0.0])[0]
     specific_gas_constant = np.asarray(gram.data.Density_kgm3[gram.data.Height_km <= 0.0])[0]
