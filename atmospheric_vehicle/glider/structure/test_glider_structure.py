a05570a84cc20e861acbb9e6600b6a779d7ad0ee
DPElia <e.dallapiazza@student.tudelft.nl>
2023-06-14 17:17:13 +0200

    Update test_glider_structure.py
    first unit test implemented

--- atmospheric_vehicle/glider/structure/test_glider_structure.py
+++ atmospheric_vehicle/glider/structure/test_glider_structure.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
+from atmospheric_vehicle.glider.structure.main import *
 
+c_w_root = 0.785  # [m], root chord, given from Luigi
+c_w_tip = 0.713  # [m], tip chord, given from Luigi
+b_w = 6  # [m], wing span, given from Luigi
+c_w = calculate_c_w(c_w_root, c_w_tip, b_w)[0]  # array of chord values that will be used for later estimations
+b_range = calculate_c_w(c_w_root, c_w_tip, b_w)[1]
+db = calculate_c_w(c_w_root, c_w_tip, b_w)[2]
 
 class GliderStructureTests(TestCase):
-    def test_calculate_c_w(self):
-        self.fail()
+    def test_calculate_c_w1(self):
+        self.assertEqual(len(c_w), len(b_range))
