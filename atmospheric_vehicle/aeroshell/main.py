004ec8649686bf5f66eecadba1254244f2c27dc1
felix <felix@lagarden.de>
2023-06-14 10:47:53 +0200

    started merging tools

--- atmospheric_vehicle/aeroshell/main.py
+++ atmospheric_vehicle/aeroshell/main.py
@@ -1,4 +1,10 @@
 
 
+class Aeroshell():
+
+    def __init__(self, target_pressure, baseline_mass, glider_mass, diameter, capsule_c_d, parachute_c_ds,
+                 shock_load_factors, deploy_limit_load, adcs_power, pr_power, delta_T, alt, lat, lon, speed,
+                 flight_path_angle, heading_angle,):
+
 if __name__ == "__main__":
     print("Hello World")
