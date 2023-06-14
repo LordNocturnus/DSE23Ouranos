0497d498f6a1e2bcccaa0d06b7aef2e0a07196d5
snuijten <snuijten@tudelft.nl>
2023-06-14 12:19:27 +0200

    different battery data

--- atmospheric_vehicle/glider/power/main.py
+++ atmospheric_vehicle/glider/power/main.py
@@ -1,15 +1,18 @@
+import math as m
 dod = 1
 n_bat = 0.9
 n_cab = 0.9
-vol_energy = 1653.65e3
-spec_energy = 711.3 #https://www.greencarcongress.com/2023/04/20230429-li.html
-margin = 0.5
+vol_energy = 1000e3
+spec_energy = 710 #https://www.greencarcongress.com/2023/04/20230429-li.html
+margin = 0.2
 days=3
+nom_watt = 0.5
+capacity = 19*2
 
-dh_p_req = 40
-ttc_p_req = 10
-adcs_p_req = 10
-pl_p_req = 90
+dh_p_req = 0
+ttc_p_req = 20
+adcs_p_req = 0
+pl_p_req = 96.1
 tm_p_req = 30
 
 def f_tot_p_req(dh_p_req, ttc_p_req,
@@ -29,13 +32,16 @@ def f_v_bat(tot_p_req, t, dod, n_bat, n_cab, vol_energy):
     v_bat = tot_p_req * t / dod / n_bat / n_cab / vol_energy
     return v_bat
 
+def f_n_bat(tot_p_req, t, dod, n_bat, n_cab, capacity):
+    n_bat = m.ceil(tot_p_req * t / dod / n_bat / n_cab / capacity)
+    return n_bat
+
 if __name__ == "__main__":
     tot_p_req = f_tot_p_req(dh_p_req, ttc_p_req, adcs_p_req, pl_p_req, tm_p_req)
     t = f_t(days)
-    m_bat = f_m_bat(tot_p_req, t, dod, n_bat, n_cab, spec_energy)
-    m_bat = m_bat*(1+margin)
-    v_bat = f_v_bat(tot_p_req, t, dod, n_bat, n_cab, spec_energy)
-    v_bat = v_bat*(1+margin)
-    print("Required energy [kWh]:", tot_p_req*t/1000)
-    print("Battery mass [kg]:", m_bat)
-    print("Battery mass [kg]:", v_bat)
+    print("Required energy [kWh]:", tot_p_req * t / dod / n_bat / n_cab/1000)
+    n_bat = f_n_bat(tot_p_req, t, dod, n_bat, n_cab, capacity)
+    print("Battery number [n]:", n_bat)
+    print("Battery volume [m3]:", ((33e-3)/2)**2*m.pi*59e-3*n_bat)
+    print("Battery mass [kg]:", n_bat * 72e-3)
+
