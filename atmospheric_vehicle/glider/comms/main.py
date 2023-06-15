7e43b32abd7bb2a93dc707ca093258cb19524268
snuijten <snuijten@tudelft.nl>
2023-06-15 12:52:40 +0200

    updated values

--- atmospheric_vehicle/glider/comms/main.py
+++ atmospheric_vehicle/glider/comms/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-n_tx = 0.18  # -
-n_rx = 0.55  # -
+n_tx = 0.933  # -
+n_rx = 0.5  # -
 
 
 def unit_to_db(unit):
@@ -48,18 +48,18 @@ if __name__ == "__main__":
     print("g_tx:", g_tx_ao)
     l_tx = unit_to_db(1 / n_tx)  # dB
     print("l_tx:", l_tx)
-    freq_ao = 400e6  # Hz
-    max_r_ao = 10000  # km
+    freq_ao = 405e6  # Hz
+    max_r_ao = 100000  # km
     l_fs_ao = l_fs(freq_ao, max_r_ao * 1000)  # dB
     print("l_fs:", l_fs_ao)
-    r_rx_ao = 0.5  # m
+    r_rx_ao = 1.3  # m
     s_rx_ao = s(r_rx_ao)  # m^2
     g_rx_ao = unit_to_db(s_rx_ao)  # dB
     print("g_rx:", g_rx_ao)
     l_rx = unit_to_db(1 / n_rx)  # dB
     print("l_rx:", l_rx)
-    l_m = unit_to_db(1 / 0.9)
-    p_tx_ao = 20  # W
+    l_m = 0*unit_to_db(1 / 0.9)
+    p_tx_ao = 2*10.7  # W
     p_tx_ao = unit_to_db(p_tx_ao)  # dB
     print("p_tx:", p_tx_ao)
     p_rx_ao = p_rx(p_tx_ao, g_tx_ao, l_tx, l_fs_ao, l_m, g_rx_ao, l_rx)
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     e_b_ao = e_b(db_to_unit(p_rx_ao), bit_rate_ao)
     print("e_b:", e_b_ao)
     print("e_b [dB]:", unit_to_db(e_b_ao))
-    n_0 = n_0(103.15)
+    n_0 = n_0(103.34)
     print("n_0:", n_0)
     print("n_0 [dB]:", unit_to_db(n_0))
     energy_per_bit_criteria(e_b_ao, n_0)
