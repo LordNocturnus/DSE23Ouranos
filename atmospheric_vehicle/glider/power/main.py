841af3879a4036693938fb891360e83dc1b9e72f
snuijten <snuijten@tudelft.nl>
2023-06-16 14:40:18 +0200

    updated values

--- atmospheric_vehicle/glider/power/main.py
+++ atmospheric_vehicle/glider/power/main.py
@@ -5,15 +5,15 @@ n_cab = 0.9
 vol_energy = 1000e3
 spec_energy = 710 #https://www.eaglepicher.com/sites/default/files/LCF-514%200222.pdf
 margin = 0.2
-days=1+8/24+33/60/24
+hours=12
 nom_watt = 0.5
 capacity = 19*2 # Watt
 
 dh_p_req = 0
-ttc_p_req = 85.6
+ttc_p_req = 128.4
 adcs_p_req = 0
 pl_p_req = 96.1
-tm_p_req = 30
+tm_p_req = 0
 aero_p_req =1
 
 
@@ -21,11 +21,6 @@ def f_tot_p_req(dh_p_req, ttc_p_req,
                 adcs_p_req, pl_p_req, tm_p_req, aero_p_req):
     tot_p_req = dh_p_req + ttc_p_req + adcs_p_req + pl_p_req + tm_p_req + aero_p_req
     return tot_p_req
-
-
-def f_t(days):
-    t = days * 24
-    return t
 
 
 def f_m_bat(tot_p_req, t, dod, n_bat, n_cab, spec_energy):
@@ -44,7 +39,7 @@ def f_n_bat(tot_p_req, t, dod, n_bat, n_cab, capacity):
 
 if __name__ == "__main__":
     tot_p_req = f_tot_p_req(dh_p_req, ttc_p_req, adcs_p_req, pl_p_req, tm_p_req, aero_p_req)
-    t = f_t(days)
+    t = hours
     print("Required energy [kWh]:", tot_p_req * t / dod / n_bat / n_cab/1000)
     n_bat = f_n_bat(tot_p_req, t, dod, n_bat, n_cab, capacity)
     print("Battery number [n]:", n_bat)
